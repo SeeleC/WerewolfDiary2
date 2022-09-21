5bab9248776ed893a15ae3cab0ad846d9b05a329
SeeleC <1352510038@qq.com>
2022-09-21 20:10:40 +0800

    upload files

---
+++ config.py
@@ -0,0 +1,11 @@
+default_data = {
+    'username': '',
+    'health': 5,
+    'backpack': [],
+    'location': '',
+    'day': 0,
+    'daytime': 0,
+    'active_balance': 6
+}
+
+time_tips = ["早上8点", "早上10点", "正午12点", "下午3点", "下午5点", "晚上7点", "晚上10点"]
